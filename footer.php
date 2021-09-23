dab8f27841f2bf3e601133e65ee504a8d49d51a3
eds4444 <efimov0204@gmail.com>
2021-09-23 18:39:21 +0300

    js

--- footer.php
+++ footer.php
@@ -1,38 +1,54 @@
-   <!-- footer
-   ================================================== -->
-   <footer>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+   </head>
+      <body>
+             
+          <!-- footer
+            ================================================== -->
+            <footer>
 
-      <div class="row">
+               <div class="row">
 
-         <div class="twelve columns">
+                  <div class="twelve columns">
 
-            <ul class="footer-nav">
-					<li><a href="#">Home.</a></li>
-              	<li><a href="#">Blog.</a></li>
-              	<li><a href="#">Portfolio.</a></li>
-              	<li><a href="#">About.</a></li>
-              	<li><a href="#">Contact.</a></li>
-               <li><a href="#">Features.</a></li>
-			   </ul>
+                     <ul class="footer-nav">
+                        <li><a href="#">Home.</a></li>
+                        <li><a href="#">Blog.</a></li>
+                        <li><a href="#">Portfolio.</a></li>
+                        <li><a href="#">About.</a></li>
+                        <li><a href="#">Contact.</a></li>
+                        <li><a href="#">Features.</a></li>
+                     </ul>
 
-            <ul class="footer-social">
-               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-               <li><a href="#"><i class="fa fa-skype"></i></a></li>
-               <li><a href="#"><i class="fa fa-rss"></i></a></li>
-            </ul>
+                     <ul class="footer-social">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                     </ul>
 
-            <ul class="copyright">
-               <li>Copyright &copy; 2014 Sparrow</li> 
-               <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>               
-            </ul>
+                     <ul class="copyright">
+                        <li>Copyright &copy; 2014 Sparrow</li> 
+                        <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>               
+                     </ul>
 
-         </div>
+                  </div>
 
-         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+                  <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
 
-      </div>
+               </div>
 
-   </footer> <!-- Footer End-->
+            </footer> <!-- Footer End-->
+
+            
+
+            <?php wp_footer();?>
+      </body>
+</html>
--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'style_theme');
+add_action( 'wp_enqueue_scripts', 'theme_styles');
+add_action( 'wp_footer', 'theme_scripts');
 
-function style_theme() {
+
+function theme_styles() {
     wp_enqueue_style( 'style', get_stylesheet_uri());
     wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css' );
     wp_enqueue_style( 'fonts', get_template_directory_uri() . '/assets/css/fonts.css' );
@@ -11,6 +13,16 @@ function style_theme() {
     wp_enqueue_style( '404', get_template_directory_uri() . '/assets/css/404.css' );
 
 
+}
+
+function theme_scripts() {
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js' );
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js',['jquery'], null, true);
+    wp_enqueue_script( 'doubletaptogo', get_template_directory_uri() . '/assets/js/jquery.doubletaptogo.js',['jquery'], null, true);
+    wp_enqueue_script( 'init', get_template_directory_uri() . '/assets/js/init.js',['jquery'], null, true);
+    //wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', null, null, false);
 }
 
 ?>
