4a4210eca62265f74025d9ca7365f7f01416dfc3
eds4444 <efimov0204@gmail.com>
2021-10-17 14:07:08 +0300

    pagination

--- category.php
+++ category.php
@@ -58,97 +58,13 @@
             </div>
 
         </article> <!-- post end -->
-        <?php    
-            }
-        }  ?>
 
-        <article class="post">
+        <?php  } //end while ?>
 
-            <div class="entry-header cf">
+        <?php  the_posts_pagination(); //Выводит на экран ссылки пагинации ?>
 
-                <h1><a href="single.html" title="">Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin auctor.</a></h1>
+        <?php  } //end if ?>
 
-                <p class="post-meta">
-
-                    <time class="date" datetime="2014-01-14T11:24">Jan 14, 2013</time>
-                    /
-                    <span class="categories">
-                    <a href="#">Design</a> /
-                    <a href="#">User Inferface</a> /
-                    <a href="#">Web Design</a>
-                    </span>
-
-                </p>
-
-            </div>
-
-            <div class="post-thumb">
-                <a href="single.html" title=""><img src="images/post-image/post-image-1300x500-02.jpg" alt="post-image" title="post-image"></a>
-            </div>
-
-            <div class="post-content">
-
-                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
-                nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
-                cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
-                ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. </p>
-
-            </div>
-
-        </article> <!-- post end -->
-
-        <article class="post">
-
-            <div class="entry-header cf">
-
-                <h1><a href="single.html" title="">Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin auctor.</a></h1>
-
-                <p class="post-meta">
-
-                    <time class="date" datetime="2014-01-14T11:24">Jan 14, 2014</time>
-                    /
-                    <span class="categories">
-                    <a href="#">Design</a> /
-                    <a href="#">User Inferface</a> /
-                    <a href="#">Web Design</a>
-                    </span>
-
-                </p>
-
-            </div>
-
-            <div class="post-thumb">
-                <a href="single.html" title=""><img src="images/post-image/post-image-1300x500-03.jpg" alt="post-image" title="post-image"></a>
-            </div>
-
-            <div class="post-content">
-
-                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
-                nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
-                cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
-                ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. </p>
-
-            </div>
-
-        </article> <!-- post end -->
-        
-
-        <!-- Pagination -->
-        <nav class="col full pagination">
-                <ul>
-                <li><span class="page-numbers prev inactive">Prev</span></li>
-                    <li><span class="page-numbers current">1</span></li>
-                    <li><a href="#" class="page-numbers">2</a></li>
-                <li><a href="#" class="page-numbers">3</a></li>
-                <li><a href="#" class="page-numbers">4</a></li>
-                <li><a href="#" class="page-numbers">5</a></li>
-                <li><a href="#" class="page-numbers">6</a></li>
-                <li><a href="#" class="page-numbers">7</a></li>
-                <li><a href="#" class="page-numbers">8</a></li>
-                <li><a href="#" class="page-numbers">9</a></li>
-                    <li><a href="#" class="page-numbers next">Next</a></li>
-                </ul>
-            </nav>
 
         </div> <!-- Primary End-->
 
--- functions.php
+++ functions.php
@@ -16,6 +16,28 @@ function theme_register_nav_menu() {
         global $post;
         return '<a href="'. get_permalink($post) . '">  Читать дальше</a>';
     }
+    // удаляет H2 из шаблона пагинации
+    add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+    function my_navigation_template( $template, $class ){
+        /*
+        Вид базового шаблона:
+        <nav class="navigation %1$s" role="navigation">
+            <h2 class="screen-reader-text">%2$s</h2>
+            <div class="nav-links">%3$s</div>
+        </nav>
+        */
+
+        return '
+        <nav class="navigation %1$s" role="navigation">
+            <div class="nav-links">%3$s</div>
+        </nav>    
+        ';
+    }
+
+    // выводим пагинацию
+    the_posts_pagination( array(
+        'end_size' => 2,
+    ) ); 
 
 }
 
