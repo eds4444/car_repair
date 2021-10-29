10e933854f4c0c73d0e0980d8871197d8f15d735
eds4444 <efimov0204@gmail.com>
2021-10-29 18:45:05 +0300

    acf

--- portfolio-post.php
+++ portfolio-post.php
@@ -5,4 +5,117 @@ Template Post Type: portfolio
 */
 ?>
 
-Работа из портфолио
+   <?php get_header();  ?>
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row portfolio">
+
+         <section class="entry cf">
+
+            <div id="secondary"  class="four columns entry-details">
+
+                  <h1>Geometrics.</h1>
+
+                  <div class="entry-description">
+
+                     <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                     nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
+                     cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
+                     ornare odio.</p>
+
+                  </div>
+
+                  <ul class="portfolio-meta-list">
+						   <li><span>Date: </span><?php the_field('project-date'); // подключение кастомных полей ?></li>
+						   <li><span>Client </span><?php the_field('client'); // подключение кастомных полей ?></li>
+						   <li><span>Skills: </span><?php the_field('skills'); // подключение кастомных полей ?></li>
+				      </ul>
+
+                  <a class="button" href="http://behance.net">View project</a>
+
+            </div> <!-- secondary End-->
+
+            <div id="primary" class="eight columns">
+
+               <div class="entry-media">
+
+                  <img src="<?php the_field('project-photo'); // подключение кастомных полей ?>" alt="">
+
+                  <img src="images/portfolio/entries/geometric-backgrounds-02.jpg" alt="" />
+
+               </div>
+
+               <div class="entry-excerpt">
+
+				      <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                  nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
+                  cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
+
+					</div>
+
+            </div> <!-- primary end-->
+
+         </section> <!-- end section -->
+
+         <ul class="post-nav cf">
+			   <li class="prev"><a href="#" rel="prev"><strong>Previous Entry</strong> Duis Sed Odio Sit Amet Nibh Vulputate</a></li>
+				<li class="next"><a href="#" rel="next"><strong>Next Entry</strong> Morbi Elit Consequat Ipsum</a></li>
+			</ul>
+
+      </div>
+
+   </div> <!-- content End-->
+
+   <!-- Tweets Section
+   ================================================== -->
+   <section id="tweets">
+
+      <div class="row">
+
+         <div class="tweeter-icon align-center">
+            <i class="fa fa-twitter"></i>
+         </div>
+
+         <ul id="twitter" class="align-center">
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">2 Days Ago</a></b>
+            </li>
+            <!--
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">3 Days Ago</a></b>
+            </li>
+            -->
+         </ul>
+
+         <p class="align-center"><a href="#" class="button">Follow us</a></p>
+
+      </div>
+
+   </section> <!-- Tweet Section End-->
+
+ <?php  get_footer(); ?>
