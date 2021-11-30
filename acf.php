ad81d7ca17fc35d5b79ed4ddae6c1c9d30e5d8eb
eds4444 <efimov0204@gmail.com>
2021-11-30 18:43:18 +0200

    126

---
+++ acf.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: Портфолио
+*/
+?>
+<?php get_header(); ?>
--- portfolio.php
+++ portfolio.php
@@ -12,7 +12,7 @@ Template Name: Портфолио
 
          <div class="ten columns centered text-center">
 
-            <h1>Oдлтдтдтдлтлд </h1>
+            <h1>O </h1>
 
 
          </div>
